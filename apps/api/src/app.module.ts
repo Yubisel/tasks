5db8ac362275602b7919d5459c1cb449a3fe8a89
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2023-07-18 15:27:28 -0300

    created resource tasks

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,16 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../client/dist'),
     }),
+    TasksModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
