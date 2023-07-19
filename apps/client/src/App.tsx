531d8ebdeacbb21e9608498205e3801e71218b47
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2023-07-19 17:40:53 -0300

    added tailwind to the client

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,31 +1,19 @@
 // import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 
 function App() {
   // const [count, setCount] = useState(0);
-  const apiCall = async () => {
-    const response = await fetch("/api");
-    const text = await response.text();
-    console.log(text);
-  };
+  // const apiCall = async () => {
+  //   const response = await fetch("/api");
+  //   const text = await response.text();
+  //   console.log(text);
+  // };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => {void apiCall()}}>Api call</button>
-      </div>
-    </>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   );
 }
 
