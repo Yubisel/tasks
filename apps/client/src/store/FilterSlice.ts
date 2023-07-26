3c6fb4217931d2a5824cc94bef18f67b8bd00025
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2023-07-26 17:40:12 -0300

    added filter slice

---
+++ apps/client/src/store/FilterSlice.ts
@@ -0,0 +1,18 @@
+import { StateCreator } from "zustand";
+import { TStore } from "./index";
+import { type TFilterOption, FILTER_OPTIONS } from "../types";
+
+export type TFilter = {
+  selectedFilter: TFilterOption;
+  setSelectedFilter: (selected: TFilterOption) => void;
+};
+
+const FilterSlice: StateCreator<TStore, [], [], TFilter> = (set) => ({
+  selectedFilter: FILTER_OPTIONS.ALL,
+  setSelectedFilter: (selected: TFilterOption) =>
+    set(() => ({
+      selectedFilter: selected,
+    })),
+});
+
+export default FilterSlice;
