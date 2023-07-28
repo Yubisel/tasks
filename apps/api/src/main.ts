66ba1d546b502ba6cc7a89abb72653a10fc79781
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2023-07-27 22:57:43 -0300

    added config validation to the api and some new env variables

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.setGlobalPrefix('api/v1.0', { exclude: ['docs'] });
 
   // Validation
@@ -29,6 +31,10 @@ async function bootstrap() {
     // customCssUrl: '/custom.css',
   });
 
-  await app.listen(3000);
+  await app.listen(configService.get('API_PORT'), () =>
+    console.log(
+      `Server up and listen on port ${configService.get('API_PORT')}`,
+    ),
+  );
 }
 bootstrap();
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    ...(process.env.VITE_CLIENT_PORT && {port: +process.env.VITE_CLIENT_PORT}),
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true
-      }
-    }
-  }
-})
+      "/api": {
+        target: `http://localhost:${process.env.API_PORT || 3000}/`,
+        changeOrigin: true,
+      },
+    },
+  },
+});
