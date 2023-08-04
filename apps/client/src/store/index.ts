42c64629a12dc40e0a60387789711df2f098989b
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2023-08-04 09:48:03 -0300

    added dockerfile for production

--- apps/client/src/store/index.ts
+++ apps/client/src/store/index.ts
@@ -15,7 +15,7 @@ const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
   const store = _store as WithSelectors<typeof _store>;
   store.use = {};
   for (const k of Object.keys(store.getState())) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
     (store.use as any)[k] = () => store((s) => s[k as keyof typeof s]);
   }
 
@@ -31,5 +31,5 @@ const useStoreBase = create<TStore>((...args) => ({
   ...FilterSlice(...args),
 }));
 
-const useStore = createSelectors(useStoreBase);
-export default useStore;
+const useStorea = createSelectors(useStoreBase);
+export default useStorea;
