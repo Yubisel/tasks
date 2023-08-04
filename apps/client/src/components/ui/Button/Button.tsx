c6a556b78d25b2a178fe14b96e81d858bd81c68a
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2023-08-04 14:04:11 -0300

    added button disabled status

--- apps/client/src/components/ui/Button/Button.tsx
+++ apps/client/src/components/ui/Button/Button.tsx
@@ -26,7 +26,6 @@ interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: () => void;
   variant?: TButtonVariant;
   className?: string;
-  [key: string]: unknown;
 }
 
 const Button: React.FC<React.PropsWithChildren<IProps>> = ({
@@ -39,7 +38,7 @@ const Button: React.FC<React.PropsWithChildren<IProps>> = ({
   return (
     <button
       onClick={onClick}
-      className={`relative inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${VARIANT_VALUES[variant]} ${className || ""}`}
+      className={`relative inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-gray-500 ${VARIANT_VALUES[variant]} ${className || ""}`}
       {...props}
     >
       {children}
