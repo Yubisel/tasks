ff119da0af67e9accddf0f9cdde25233f5bc6bfb
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2023-08-04 22:53:59 -0300

    changed consolo.log with logger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
   const configService = app.get(ConfigService);
   app.setGlobalPrefix('api/v1.0', { exclude: ['docs'] });
 
@@ -32,7 +33,7 @@ async function bootstrap() {
   });
 
   await app.listen(configService.get('PORT'), () =>
-    console.log(`Server up and listen on port ${configService.get('PORT')}`),
+    logger.log(`Server up and listen on port ${configService.get('PORT')}`),
   );
 }
 bootstrap();
